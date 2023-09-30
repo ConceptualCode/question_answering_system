909d1c2759bf565602c61e184bf217edfbd76fd0
Anthony Soronnadi <soronnadianthony@gmail.com>
2023-09-30 13:47:44 +0100

    Implemented a main.py file for asking questions and getting answers

---
+++ main.py
@@ -0,0 +1,10 @@
+# Author: Anthony Soronnadi
+from retrieval import Retriever
+
+def main():
+    retriever = Retriever()
+    question = input('Enter the question: ')
+    retriever.run(question)
+
+if __name__ == "__main__":
+    main()
