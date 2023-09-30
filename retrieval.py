891b6fc650bbdb38f072866cb861d1d90580342c
Anthony Soronnadi <soronnadianthony@gmail.com>
2023-09-30 18:37:50 +0100

    Updated the retrieval.py with the Generative AI response using gpt2

--- retrieval.py
+++ retrieval.py
@@ -6,8 +6,10 @@ import json
 import requests
 from requests.auth import HTTPBasicAuth
 from elasticsearch import Elasticsearch
-from config import ELASTICSEARCH_DETAILS, INDEX_NAME, OUTPUT_DIR, OUTPUT_FILENAME
+from config import ELASTICSEARCH_DETAILS, INDEX_NAME, OUTPUT_DIR, OUTPUT_FILENAME, GENERATIVE_ANSWERS_FILENAME
 from model import Model 
+from gen_ai import GenerativeAI
+from transformers import pipeline
 
 class Retriever:
     def __init__(self):
@@ -21,6 +23,9 @@ class Retriever:
         self.username = ELASTICSEARCH_DETAILS['username']
         self.password = ELASTICSEARCH_DETAILS['password']
         self.filename = os.path.join(OUTPUT_DIR, OUTPUT_FILENAME)
+        self.gen_ai = GenerativeAI()
+        self.gen_filename = os.path.join(OUTPUT_DIR, GENERATIVE_ANSWERS_FILENAME)
+        #self.gen_ai = pipeline('text-generation', model='gpt2')
     
     def retrieve_passages(self, question_embedding, question):
         data = {
@@ -50,39 +55,54 @@ class Retriever:
         
         return response.json()['hits']['hits']
     
-    def write_to_csv(self, rows):
-        with open(self.filename, mode='w', newline='', encoding='utf-8') as file:
+
+    def create_prompt(self, passages):
+        prompt = "Based on the following passages, ...\n"
+        for passage in passages:
+            prompt += f"{passage}\n"
+        return prompt
+        
+    def write_to_csv(self, question, retrieved_passages, generative_answer):
+        with open(self.gen_filename, mode='w', newline='', encoding='utf-8') as file:
             writer = csv.writer(file)
             writer.writerow([
                 'Question', 'Passage 1', 'Relevance Score 1', 'Passage 1 Metadata',
                 'Passage 2', 'Relevance Score 2', 'Passage 2 Metadata',
-                'Passage 3', 'Relevance Score 3', 'Passage 3 Metadata'
+                'Passage 3', 'Relevance Score 3', 'Passage 3 Metadata',
+                'Generative AI Answer'
             ])
-            writer.writerows(rows)
+            
+            row = [question]
+            for passage in retrieved_passages:
+                # appending Passage, Relevance Score, and Metadata to the row
+                row.extend([passage['passage'], passage['score'], passage['metadata']])
+            
+            row.append(generative_answer)
+            writer.writerow(row)
+        print(f"Data has been written to {self.gen_filename}")
     
     def run(self, question):
         model = Model()
         question_embedding = model.model.encode([question])[0].tolist()
-        
         hits = self.retrieve_passages(question_embedding, question)
         
-        rows = []
+        retrieved_passages = []
         if hits:
-            row = [question]
             for hit in hits:
                 passage = hit['_source'].get('Passage', '')
                 metadata = hit['_source'].get('Metadata', {})
                 relevance_score = hit['_score']
-                row.extend([passage, relevance_score, metadata])
-            rows.append(row)
-        
-        if rows:
-            self.write_to_csv(rows)
-            print(f"Data has been written to {self.filename}")
+                retrieved_passages.append({'passage': passage, 'score': relevance_score, 'metadata': metadata})
+            
+            prompt = self.create_prompt(retrieved_passages)
+            generative_answer = self.gen_ai.generate_answer(prompt)
+            
+            # Write data to CSV
+            self.write_to_csv(question, retrieved_passages, generative_answer)
         else:
             print("No data to write.")
 
-if __name__ == "__main__":
-    retriever = Retriever()
-    question = input('Enter the question: ')
-    retriever.run(question)
+# if __name__ == "__main__":
+#     retriever = Retriever()
+#     question = input('Enter the question: ')
+#     retriever.run(question)
