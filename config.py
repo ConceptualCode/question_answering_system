3036ed6234719d9cc5fe61af29284f30bfc7aa2f
Anthony Soronnadi <soronnadianthony@gmail.com>
2023-09-30 04:20:26 +0100

    Initial commit with parsing and config files

---
+++ config.py
@@ -0,0 +1,12 @@
+# Author: Anthony Soronnadi
+
+ELASTICSEARCH_DETAILS = {
+    'url': 'https://localhost:9200',
+    'username': 'elastic',
+    'password': '',
+}
+
+CORPUS_DIR = 'path_to_corpus_dir'
+INDEX_NAME = 'passage_metadata_embeddings1_index'
+OUTPUT_FILENAME = 'questions_answers1.csv'
+MODEL_NAME = 'paraphrase-MiniLM-L6-v2'
